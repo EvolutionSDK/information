9bcf48f53fe016b1d5857fbabcec344adfe88051
KellyLSB <kellylsbkr@gmail.com>
2012-02-23 13:03:04 -0700

    information bundle (new style)

--- _bundle.php
+++ _bundle.php
@@ -2,6 +2,7 @@
 
 namespace Bundles\Information;
 use Bundles\SQL\SQLBundle;
+use Bundles\SQL\Model;
 use Exception;
 use e;
 
@@ -12,21 +13,17 @@ use e;
  */
 class Bundle extends SQLBundle {
 	
-	public function __callBundle($model) {
-		if(is_object($model))
-			$model = $model->__map();
-		list($table, $id) = explode(':', $model);
-		return new Accessor($table, (int) $id);
+	public function __callBundle() {
+		return $this;
 	}
 
-	public function core($id = 1) {
-		return new Accessor('core.information', $id);
+	public function __getBundle($method = true) {
+		return new Accessor('information.record', 'core');
 	}
 
-	public function core64($id = 1) {
-		$a64 = new Accessor('core.information', $id);
-		$a64->use_base64 = true;
-		return $a64;
+	public function accessor(Model $model) {
+		$tableName = "\$information ".$model->__getTable();
+		return new Accessor($tableName, $model->id);
 	}
 
 }
@@ -38,41 +35,49 @@ class Bundle extends SQLBundle {
  */
 class Accessor {
 	
+	private $base64 = false;
 	private $table;
 	private $id;
-	public $use_base64 = false;
 
 	public function __construct($table, $id) {
-		if(!is_numeric($id) || $id < 1)
-			throw new Exception("ID `$id` is invalid, must be a positive integer");
 		$this->table = $table;
 		$this->id = $id;
 	}
 
+	public function b64() {
+		$this->base64 = true;
+		return $this;
+	}
+
 	public function getRecord($field) {
 		$field = mysql_escape_string($field);
-		return e::$sql->query("SELECT `field`, `value` FROM `information.record` WHERE `model` = '$this->table' AND `id` = '$this->id' AND `field` = '$field'")->row();
+		$owner = is_numeric($this->id) ? " AND `owner` = '$this->id'" : '';
+		return e::$sql->query("SELECT `field`, `value` FROM `$this->table` WHERE `field` = '$field' $owner")->row();
 	}
 
 	public function listRecords() {
-		return e::$sql->query("SELECT `field`, `value`, `updated_timestamp`, `created_timestamp` FROM `information.record` WHERE `model` = '$this->table' AND `id` = '$this->id'")->all();
+		$owner = is_numeric($this->id) ? " WHERE `owner` = '$this->id'" : '';
+		return e::$sql->query("SELECT `field`, `value`, `updated_timestamp` FROM `$this->table` $owner")->all();
 	}
 
 	public function delRecord($field) {
 		$field = mysql_escape_string($field);
-		return e::$sql->query("DELETE FROM `information.record` WHERE `model` = '$this->table' AND `id` = '$this->id' AND `field` = '$field'");
+		$owner = is_numeric($this->id) ? " AND `owner` = '$this->id'" : '';
+		return e::$sql->query("DELETE FROM `$this->table` WHERE `field` = '$field' $owner");
 	}
 
 	public function updateRecord($field, $value) {
 		$field = mysql_escape_string($field);
 		$value = mysql_escape_string($value);
-		return e::$sql->query("UPDATE `information.record` SET `value` = '$value' WHERE `model` = '$this->table' AND `id` = '$this->id' AND `field` = '$field'");
+		$owner = is_numeric($this->id) ? " AND `owner` = '$this->id'" : '';
+		return e::$sql->query("UPDATE `$this->table` SET `value` = '$value' WHERE `field` = '$field' $owner");
 	}
 
 	public function createRecord($field, $value) {
 		$field = mysql_escape_string($field);
 		$value = mysql_escape_string($value);
-		return e::$sql->query("INSERT INTO `information.record` (`model`, `id`, `field`, `value`) VALUES ('$this->table', $this->id, '$field', '$value')");
+		$owner = is_numeric($this->id) ? ", `owner` = '$this->id'" : '';
+		return e::$sql->query("INSERT INTO `$this->table` SET `field` = '$field', `value` = '$value' $owner");
 	}
 
 	public function __call($method, $args) {
@@ -86,7 +91,7 @@ class Accessor {
 	public function __get($field) {
 		$row = $this->getRecord($field);
 		if(!$row) return null;
-		if($this->use_base64)
+		if($this->base64)
 			return base64_encode($row['value']);
 		return $row['value'];
 	}
--- extend/sql/library/extensions/information.php
+++ extend/sql/library/extensions/information.php
@@ -8,16 +8,39 @@ use e;
 
 class Information {
 
+	public function _buildTable($tableName) {
+		e::$sql->architect("\$information $tableName", array(
+			'fields' => array(
+				"id" => '_suppress',
+				"created_timestamp" => '_suppress',
+				"owner" => 'number',
+				"updated_timestamp" => array(
+					'Type' => 'timestamp',
+					'Null' => 'YES',
+					'Key' => '',
+					'Default' => 'CURRENT_TIMESTAMP',
+					'Extra' => 'on update CURRENT_TIMESTAMP'
+				),
+				"field" => 'string',
+				"value" => 'string'
+			)
+		));
+	}
+
 	public function modelSet(Model $model, $key, $val) {
-		e::information($model)->$key = $val;
+		e::information()->accessor($model)->$key = $val;
 	}
 
 	public function modelGet(Model $model, $key) {
-		return e::information($model)->$key;
+		return e::information()->accessor($model)->$key;
+	}
+
+	public function modelGet64(Model $model, $key) {
+		return e::information()->accessor($model)->b64()->$key;
 	}
 
 	public function modelList(Model $model) {
-		return e::information($model)->listRecords();	
+		return e::information()->accessor($model)->listRecords();
 	}
 
 }
